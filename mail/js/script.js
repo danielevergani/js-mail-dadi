5e6aad6da7d2da66b02cc2d77f2f11fd3fc85dd0
danielevergani <dvergani94@gmail.com>
2021-03-01 14:55:19 +0100

    terminato es mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,20 @@
-alert("ciao");
+// verificare se email inserita da utente corrisponde a mail in un array
+// 1 creo variabile x mail e array
+var mailPersonale = prompt("inserisci la tua email");
+var elencoMail = ["daniele@gmail.com", "paolo@gmail.com", "francescoe@gmail.com", "giuseppe@gmail.com"];
+
+console.log(mailPersonale);
+console.log(elencoMail);
+
+// 2 ciclo for con if annidato per fare verifica nell'array
+
+for (i=0; i<elencoMail.length; i++){
+    if (mailPersonale == elencoMail[i]){
+        console.log("sei autorizzato");
+        break;
+    }
+}
+
+if (i == elencoMail.length){
+    console.log("la tua email non è presente");
+}
