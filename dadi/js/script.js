9e6e73a144912907bd240c3566e8d7cd6b43e1e6
danielevergani <dvergani94@gmail.com>
2021-03-01 15:36:24 +0100

    inserito ciclo for in es dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,16 +1,31 @@
 // inizializzo variabili x entrambi i giocatori
 
-var mioNumero = Math.floor(Math.random()*6) + 1;
-console.log(mioNumero);
-var numeroSfidante = Math.floor(Math.random()*6) + 1;
-console.log(numeroSfidante);
+condizione = "";
 
-if (mioNumero>numeroSfidante){
-    console.log("hai vinto");
-}
-else if (mioNumero<numeroSfidante){
-    console.log("hai perso");
-}
-else {
-    console.log("pareggio");
+for (i=0; i<100; i++) {
+
+    var mioNumero = Math.floor(Math.random()*6) + 1;
+    console.log(mioNumero);
+    var numeroSfidante = Math.floor(Math.random()*6) + 1;
+    console.log(numeroSfidante);
+
+    if (mioNumero>numeroSfidante){
+        console.log("hai vinto");
+    }
+    else if (mioNumero<numeroSfidante){
+        console.log("hai perso");
+    }
+    else {
+        console.log("pareggio");
+    }
+
+    condizione = prompt("vuoi smettere di giocare?");
+
+    if (condizione == "si"){
+        break;
+    }
+    else{
+        continue;
+    }
+
 }
